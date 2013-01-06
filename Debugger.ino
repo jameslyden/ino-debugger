61168ca0a487df48c2f2a942564932f9b3dd5231
James Lyden <james@lyden.org>
2013-01-06 01:05:00 -0800

    Initial commit of Arduino component of Visual Debugger

---
+++ Debugger.ino
@@ -0,0 +1,73 @@
+/* A drop-in debugging routine to add to Arduino projects.
+ * Setup requires configuring the serial port as needed, adding the Debug function to the sketch,
+ * adding the global variables, and calling Debug() from somewhere in the body of loop()
+ *
+ * This sketch can also be used as-is, in which case it provides near-realtime I/O status,
+ * with no additional functionality.
+ */
+
+int currDebugTime;
+int lastDebugTime;
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  // do work
+  delay(1000);
+  // collect pin states and send to serial port 
+  Debug();  
+}
+
+void Debug() {
+  // setup analog and digital pin ranges
+  int aPinStart = A0;
+  int aPinEnd = A3;
+  int dPinStart = 2;
+  int dPinEnd = 5;
+  // all debug data is assembled into a char array before writing
+  // structure is 5B header, 1B per analog pin, 1B per digital pin, an 1B CRC value
+  int debugPayloadSize = (aPinEnd - aPinStart + 1) + (dPinEnd - dPinStart + 1);
+  int debugDataSize = 5 + debugPayloadSize + 1;
+  int debugDataCount = 0;
+  char debugData[debugDataSize];
+
+  // Start with a header, should prevent accidental interpretation of other program data as debug data
+  debugData[debugDataCount++] = 255;
+  debugData[debugDataCount++] = 254;
+  debugData[debugDataCount++] = 253;
+  debugData[debugDataCount++] = debugDataSize;
+  
+  // Compute ms since last run through this function
+  currDebugTime = millis();
+  int interval = currDebugTime - lastDebugTime;
+  if (interval > 255) {
+    interval = 255;
+  }
+  debugData[debugDataCount++] = interval;
+  lastDebugTime = currDebugTime;
+  
+  // Iterate through analog inputs, mapping to a 250-point scale
+  for (int i = aPinStart; i <= aPinEnd; i++) {
+    debugData[debugDataCount++] = map(analogRead(i), 0, 1023, 0, 249);
+  }
+  
+  // Iterate through digital inputs, mapping LOW to 250 and HIGH to 251
+  for (int i = dPinStart; i <= dPinEnd; i++) {
+    debugData[debugDataCount++] = digitalRead(i) + 250; 
+  }
+  
+  // Build 8-bit CRC from payload
+  int CRC = 0;  
+  for (int i = 5; i < debugDataSize - 1; i++) {
+    CRC = CRC + debugData[i];
+    CRC = CRC % 256;
+  }
+  debugData[debugDataSize - 1] = CRC;
+  
+  // Send debug data over serial port
+  Serial.print(debugData);
+}
+  
+  
